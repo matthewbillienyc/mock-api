d977e7657f93b2ee850e809aefb2fc3647276643
HarrisonLavin <harrison.lavin@gmail.com>
2016-06-29 17:08:25 -0400

    preps for edit

--- app/api/mock_api/users.rb
+++ app/api/mock_api/users.rb
@@ -4,28 +4,36 @@ module MockAPI
 
     resource :users do
       desc 'index'
-      params do
-        # parameters go here
-      end
-      get do
-        User.all
-      end
+        params do
+          # parameters go here
+        end
+        get do
+          User.all
+        end
 
       desc 'show'
-      params do
-      end
-      get do
-        User.find(params[:id])
-        byebug
-      end
+        params do
+
+        end
+        get ':id' do
+          User.find(params[:id])
+        end
+
+      desc 'update'
+        params do
+          requires :name, type: String
+        end
+        put ":id" do
+          User.update(name: params[:name])
+        end
 
       desc 'create'
-      params do
-        requires :name, type: String
-      end
-      post do
-        User.create(name: params[:name])
-      end
+        params do
+          requires :name, type: String
+        end
+        post do
+          User.create(name: params[:name])
+        end
     end
   end
 end
