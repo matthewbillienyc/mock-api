492f610401f106d7141839d8a56eb542f4e0be0a
HarrisonLavin <harrison.lavin@gmail.com>
2016-07-01 09:52:37 -0400

    account and org stuff

--- app/api/mock_api/accounts.rb
+++ app/api/mock_api/accounts.rb
@@ -1,1 +1,17 @@
 # put the /:user_id route here, have it return the same hash for errbody, use the data_helper.rb file for dummy data
+module MockAPI
+  class Accounts < Grape::API
+    format :json
+
+    resource :accounts do
+      desc 'show'
+        params do
+        end
+
+        get ":id" do
+          @account= DataHelper.get_account_info_by_user(params[:id])
+        end
+
+    end
+  end
+end
--- app/models/data_helper.rb
+++ app/models/data_helper.rb
@@ -1,1 +1,9 @@
 # have a method to return accounts for all users
+class DataHelper
+
+  def self.get_account_info_by_user(id)
+    @acc_info = { employer: 'something', account_number: '12345', organization: { name: 'CIA', state: 'VA' } }
+    @acc_info
+  end
+
+end
